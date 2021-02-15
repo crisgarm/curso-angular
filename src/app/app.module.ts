5aca0e0277d8685ab748740839b92cbebfb19e81
crisgarm <crisgarm9@gmail.com>
2021-02-15 16:19:27 +0100

    Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { counterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-  declarations: [AppComponent, counterComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeroesModule, CounterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
