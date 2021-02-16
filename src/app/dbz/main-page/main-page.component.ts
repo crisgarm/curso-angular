15cb04b4a14cad3637cbfa1a0bb6eb0d222cc048
crisgarm <crisgarm9@gmail.com>
2021-02-16 12:21:32 +0100

    Show characters list and introduce more characters throught input

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -10,16 +10,36 @@ interface Character {
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  characters: Character[] = [
+    {
+      name: 'Goku',
+      power: 15000,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500,
+    },
+  ];
+
   new: Character = {
-    name: 'Trunks',
-    power: 14000,
+    name: '',
+    power: 0,
   };
 
-  changeName(event: any) {
-    console.log(event.target.value);
-  }
+  // changeName(event: any) {
+  //   console.log(event.target.value);
+  // }
 
   add() {
+    if (this.new.name.trim().length === 0) {
+      return;
+    }
     console.log(this.new);
+
+    this.characters.push(this.new);
+    this.new = {
+      name: '',
+      power: 0,
+    };
   }
 }
