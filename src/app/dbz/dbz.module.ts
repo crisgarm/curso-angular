dbf0fd2e3a7189c642716ca407a2cc9ea8b10fcd
crisgarm <crisgarm9@gmail.com>
2021-02-16 13:09:08 +0100

    Add child component and pass information to him

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, CharactersComponent],
   exports: [MainPageComponent],
   imports: [CommonModule, FormsModule],
 })
