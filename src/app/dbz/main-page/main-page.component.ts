0ea229e040c439a78c8763e90f2e784fc24f46b0
crisgarm <crisgarm9@gmail.com>
2021-02-17 10:10:39 +0100

    Implement the use of services in order to centralize information

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,28 +1,16 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  characters: Character[] = [
-    {
-      name: 'Goku',
-      power: 15000,
-    },
-    {
-      name: 'Vegeta',
-      power: 7500,
-    },
-  ];
-
   new: Character = {
     name: 'Maestro Roshi',
     power: 1000,
   };
 
-  addNewCharacter(argument: Character) {
-    this.characters.push(argument);
-  }
+  constructor() {}
 }
